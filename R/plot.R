a2ee285a1b807e04c0cc0e9a83bef57c7c4c7f6c
DESKTOP-89803RC\Server <neves.julien@gmail.com>
2017-02-27 23:03:42 -0500

    no message

--- R/plot.R
+++ R/plot.R
@@ -5,7 +5,7 @@
 #'
 #' @return The \code{mmc} object is returned invisibly.
 #'
-#' @inheritParams base::plot
+#' @inheritParams graphics::plot
 #' @param x An object of class "mmc"
 #' @export
 #'
@@ -13,12 +13,12 @@ plot.mmc <- function(x, ...) {
     opt_monitor <- x$opt_monitor
     alpha <- x$alpha
 
-    plot(opt_monitor$pval, xlab="Iterations",
+    graphics::plot(opt_monitor$pval, xlab="Iterations",
          ylab="P-value", main = "Evolution of mmc", ...)
-    lines(opt_monitor$max,col="green")
+    graphics::lines(opt_monitor$max,col="green")
 
     if(!is.null(alpha)){
-        lapply(alpha, function(alpha){abline(alpha,0,lty=2, col="red")})
+        lapply(alpha, function(alpha){graphics::abline(alpha,0,lty=2, col="red")})
     }
 
     invisible(x)
